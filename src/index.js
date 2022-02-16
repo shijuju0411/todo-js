a007619e9487c77597d91db83474adc246f6361c
shijuju0411 <56331579+shijuju0411@users.noreply.github.com>
2022-02-16 14:57:12 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+const onClickAdd = () => {
+  //テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  alert(inputText);
+};
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
